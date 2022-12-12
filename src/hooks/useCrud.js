a8812cbfe825e4d302777791fc59893df26a9e5f
Facundo Subercaze <faccuu.96@gmail.comm>
2022-12-12 15:22:42 -0300

    Arreglando la URL

--- src/hooks/useCrud.js
+++ src/hooks/useCrud.js
@@ -6,7 +6,7 @@ const useCrud = () => {
   const [users, setUsers] = useState()
 
   const getAllUsers = () => {
-    const URL = 'http://users-crud.academlo.tech/users/'
+    const URL = 'https://users-crud.academlo.tech/users/'
     axios.get(URL)
       .then(res => setUsers(res.data))
       .catch(err => console.log(err))
@@ -14,7 +14,7 @@ const useCrud = () => {
     
     
   const createNewUser = data => {
-    const URL = 'http://users-crud.academlo.tech/users/'
+    const URL = 'https://users-crud.academlo.tech/users/'
     axios.post(URL,data)
       .then(()=> getAllUsers())
       .catch(err => console.log(err))
@@ -27,7 +27,7 @@ const useCrud = () => {
   }
 
   const updateUserById = (id, data) => {
-    const URL = `http://users-crud.academlo.tech/users/${id}/`
+    const URL = `https://users-crud.academlo.tech/users/${id}/`
     axios.put(URL, data)
     .then(() => getAllUsers())
       .catch(err => console.log(err))
